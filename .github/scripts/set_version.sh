32acf55abac3c63b3c8bbd8aac2ae2f78bf19b7c
Tibo Beijen <tibobeijen@gmail.com>
2020-11-13 16:39:51 +0100

    Trying helm-gh-pages action.

--- .github/scripts/set_version.sh
+++ .github/scripts/set_version.sh
@@ -1,1 +1,8 @@
-echo $GIT_TAG
+GIT_TAG=0.1.test
+
+echo "Setting all chart and image versions to: ${GIT_TAG}"
+
+
+sed -i.bak "s/<<VERSION>>/$GIT_TAG/" charts/klutch/Chart.yaml
+sed -i.bak "s/<<VERSION>>/$GIT_TAG/" charts/klutch/values.yaml
+sed -i.bak "s/<<VERSION>>/$GIT_TAG/" charts/klutch-example/Chart.yaml
