f00bc4845ea857a4754775d2784dea12677b694c
Tibo Beijen <tibobeijen@gmail.com>
2020-11-13 16:45:43 +0100

    Fixed tag to test with (semver). Testing release flow on specific branch.

--- .github/scripts/set_version.sh
+++ .github/scripts/set_version.sh
@@ -1,7 +1,6 @@
-GIT_TAG=0.1.test
+GIT_TAG=0.1.1
 
 echo "Setting all chart and image versions to: ${GIT_TAG}"
-
 
 sed -i.bak "s/<<VERSION>>/$GIT_TAG/" charts/klutch/Chart.yaml
 sed -i.bak "s/<<VERSION>>/$GIT_TAG/" charts/klutch/values.yaml
