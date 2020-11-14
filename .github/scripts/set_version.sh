ac5e64b88d6ac0e680b1ee7907b6f48dd05909c9
Tibo Beijen <tibobeijen@gmail.com>
2020-11-14 15:20:42 +0100

    Using 0.0.0 placeholder instead of <<version>> to pass helm linting.

--- .github/scripts/set_version.sh
+++ .github/scripts/set_version.sh
@@ -2,6 +2,6 @@ GIT_TAG=0.1.1
 
 echo "Setting all chart and image versions to: ${GIT_TAG}"
 
-sed -i.bak "s/<<VERSION>>/$GIT_TAG/" charts/klutch/Chart.yaml
-sed -i.bak "s/<<VERSION>>/$GIT_TAG/" charts/klutch/values.yaml
-sed -i.bak "s/<<VERSION>>/$GIT_TAG/" charts/klutch-example/Chart.yaml
+sed -i.bak "s/0\.0\.0/$GIT_TAG/" charts/klutch/Chart.yaml
+sed -i.bak "s/0\.0\.0/$GIT_TAG/" charts/klutch/values.yaml
+sed -i.bak "s/0\.0\.0/$GIT_TAG/" charts/klutch-example/Chart.yaml
